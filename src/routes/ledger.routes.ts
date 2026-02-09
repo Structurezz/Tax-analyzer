63aed162a07405e02727a2bd39b74d880407d25d
Orizu <orizu1996@gmail.com>
2026-02-09 11:38:00 +0300

    new

--- src/routes/ledger.routes.ts
+++ src/routes/ledger.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { addTransaction, getTransactions, getLedgerSummary } from "../controllers/ledger.controller";
+import { addTransaction, getTransactions, getLedgerSummary } from "../controllers/ledger.controller.js";
 
 const router = Router();
 router.post("/", addTransaction);
